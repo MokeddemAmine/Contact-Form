4743bc760c5b86a28fb95a70c586b35a2fcc25a4
Amine Mokeddem <mokeddemamine1707@gmail.com>
2024-10-02 21:46:57 +0200

    redesign the contact form

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
         $name   = filter_var($_POST['name'],FILTER_SANITIZE_STRING);
         $email  = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
-        $phone  = filter_var($_POST['prenumber'].$_POST['number'],FILTER_SANITIZE_NUMBER_INT);
+        $subject  = filter_var($_POST['subject'],FILTER_SANITIZE_STRING);
         $msg    = filter_var($_POST['message'],FILTER_SANITIZE_STRING);
 
         $formErrors = array();
@@ -23,11 +23,8 @@
         if(empty($email)){
             $formErrors[] = 'email can\'t be empty';
         }
-        if(empty($_POST['number'])){
-            $formErrors[] = 'Phone number can\'t be empty';
-        }
-        if(!empty($_POST['number']) && strlen($_POST['number']) < 6){
-            $formErrors[] = 'Type a valid phone number';
+        if(empty($_POST['subject'])){
+            $formErrors[] = 'Subject can\'t be empty';
         }
         if(empty($msg)){
             $formErrors[] = 'Message can\'t be empty';
@@ -61,8 +58,8 @@
                 $mail->Subject = 'Contact Form';
                 $mail->Body    = "<p><b>From:</b> <b>$name</b></p>
                                 <p><b>Email:</b> $email</p>
-                                <p><b>Phone:</b> $phone</p>
-                                <p><b>Content:</b></p>
+                                <p><b>Subject:</b> $subject</p>
+                                <p><b>Message:</b></p>
                                 <p style='margin-left:50px'>$msg</p>";
 
                 $mail->send();
@@ -71,7 +68,7 @@
                 $name = '';
                 $email = '';
                 $msg = '';
-                $_POST['number'] = '';
+                $subject = '';
             
             } catch (Exception $e) {
                 echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
@@ -86,7 +83,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/all.min.css">
-    <link rel="stylesheet" href="css/4.5.3_bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -95,42 +92,33 @@
 </head>
 <body>
     <div class="container">
-        <form action="<?= $_SERVER['PHP_SELF']?>" method="POST" class="contact-form px-3 needs-validation" novalidate>
-            <h1 class="text-center text-capitalize my-5">contact us</h1>
-            <div class="form-group mb-3">
-                <input type="text" name="name" placeholder="Enter your name" id="" class="form-control" required pattern="[a-zA-Z ]{3,}" value="<?php if(!empty($formErrors)) echo $name; ?>"/>
-                <i class="fa-solid fa-user"></i>
-                <div class="valid-feedback">valid</div>
+        <form action="<?= $_SERVER['PHP_SELF']?>" method="POST" class="contact-form needs-validation bg-white d-block  p-5 mt-3 rounded border-white" novalidate>
+            <h1 class="text-center text-capitalize my-5 fs-4 fw-bold">contact us</h1>
+            <div class="form-group mb-4">
+                <label for="name" class="text-capitalize">name:</label>
+                <input type="text" name="name" placeholder="Enter name" id="name" class="form-control border-0 bg-light"  required pattern="[a-zA-Z ]{3,}" value="<?php if(!empty($formErrors)) echo $name; ?>"/>
+                <div class="valid-feedback">valid name</div>
                 <div class="invalid-feedback">Name must has 3 characters or more</div>
-                <span class="asterisk">*</span>
             </div>
-            <div class="form-group mb-3">
-                <input type="email" name="email" placeholder="Enter a valid email" id="" class="form-control" required value="<?php if(!empty($formErrors)) echo $email; ?>"/>
-                <i class="fa-solid fa-envelope"></i>
+            <div class="form-group mb-4">
+                <label for="email" class="text-capitalize">email:</label>
+                <input type="email" name="email" placeholder="Enter a valid email" id="email" class="form-control border-0 bg-light" required value="<?php if(!empty($formErrors)) echo $email; ?>"/>
                 <div class="valid-feedback">valid email</div>
                 <div class="invalid-feedback">Email must be valid</div>
-                <span class="asterisk">*</span>
+            </div>
+            <div class="form-group mb-4">
+                <label for="subject" class="text-capitalize">subject</label>
+                <input type="text" name="subject" placeholder="Enter a valid email" id="subject" class="form-control border-0 bg-light" required value="<?php if(!empty($formErrors)) echo $email; ?>"/>
+                <div class="valid-feedback">valid subject</div>
+                <div class="invalid-feedback">subject must be valid</div>
             </div>
             <div class="form-group mb-3">
-                <div class="input-group">
-                    <div class="input-group-prepend phone-icon">
-                        <select name="prenumber" id="" class="custom-select">
-                            <option value="+213">Algeria +213</option>
-                            <option value="+1">USA +1</option>
-                        </select>
-                    </div>
-                    <i class="fa-solid fa-phone"></i>
-                    <input type="text" name="number" placeholder="Enter you phone number" id="" class="form-control" pattern="[0-9]{6,10}" value="<?php if(!empty($formErrors)) echo $_POST['number']; ?>"/>
-                    <div class="invalid-feedback">Enter a valid number</div>
-                </div>
-            </div>
-            <div class="form-group mb-3">
-                <textarea name="message" placeholder="Enter your messade" class="form-control" id="contact-message"><?php if(!empty($formErrors)) echo $msg; ?></textarea>
+                <label for="message" class="text-capitalize">message</label>
+                <textarea name="message" placeholder="Enter your messade" class="form-control border-0 bg-light" id="message"><?php if(!empty($formErrors)) echo $msg; ?></textarea>
                 <div class="invalid-feed text-danger"></div>
-                <span class="asterisk">*</span>
             </div>
             <div class="form-group">
-                <button type="submit" class="btn btn-info btn-block text-capitalize"><i class="fa-solid fa-paper-plane"></i> send message</button>
+                <button type="submit" class="btn text-capitalize text-white" style="background: -webkit-radial-gradient(0 50%, circle, #6b69cd, #9a69bb);"><i class="fa-solid fa-paper-plane"></i> send message</button>
             </div>
             <div class="show-errors text-center">
             <?php
@@ -148,9 +136,9 @@
             </div>
         </form>
     </div>
-    <script src="js/popper.min.js"></script>
     <script src="js/jquery-3.7.0.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     <script src="js/script.js"></script>
 </body>
 </html>
